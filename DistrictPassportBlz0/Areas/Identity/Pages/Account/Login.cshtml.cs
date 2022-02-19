8bf6f0749f40d61390f7d98c261b9959e18838cf
Rh Proj <r.proj@hotmail.com>
2022-02-19 16:21:38 +0300

    DataAnnotations for Pop

--- DistrictPassportBlz0/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ DistrictPassportBlz0/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -128,7 +128,7 @@ namespace DistrictPassportBlz0.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Неверный логин или пароль");
                     return Page();
                 }
             }
